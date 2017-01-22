333a15c45a1ea24fd5c708ee83d9f4006de818d7
bastien_alv <bastien.alv@gmail.com>
2017-01-22 13:41:02 +0100

    Maj 22/01 directory

--- co/connexionBD.php
+++ co/connexionBD.php
@@ -1,3 +1,5 @@
-<?php global $connexion; try {
-    $connexion= new PDO("mysql:host=localhost; dbname=x_project", "x_project", "V4KqzFtLgyNjuFYK", array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8') );
-}catch (PDOException $e) { echo("Erreur lors de la connexion".$e->getMessage()); } ?>
+<?php global $connexion;
+    //try { $connexion= new PDO("mysql:host=sql.free.fr; dbname=epimarche", "epimarche", "rs87XsTV", array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8') ); }
+    try { $connexion= new PDO("mysql:host=localhost; dbname=x_project", "x_project", "V4KqzFtLgyNjuFYK", array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8') ); }
+    catch (PDOException $e) { echo("Erreur lors de la connexion".$e->getMessage()); }
+?>
